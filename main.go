6db47a88bd39d509ae637a50dc546b6f49c10c83
Jirayu <jirayu.saeng@gmail.com>
2025-02-15 20:52:46 -0800

    updated main.go

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
 	"bank/handler"
 	"bank/repository"
 	"bank/service"
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -29,5 +34,28 @@ func main() {
 	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", customerHandler.GetCustomerById).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", router)
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		fmt.Println("Server is running on port 8000")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println("Server is not running", err)
+		}
+	}()
+
+	<-stopChan
+	fmt.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Println("Server forced to shutdown")
+	}
+	fmt.Println("Server exiting")
 }
